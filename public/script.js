24165b469c9bf0c53478fe91c823e8d7313d9d79
GabrielCrackPro <gabrielcrackpro2001@gmail.com>
2021-06-10 14:49:03 +0200

    Basic Stats Added 💻

--- public/script.js
+++ public/script.js
@@ -11,9 +11,42 @@ const getPokemon = async (query) => {
 }
 const showPokemon = async (query) => {
     let pokemon = await getPokemon(query)
+    const pokemonData = {
+        id: pokemon.id,
+        name: pokemon.name,
+        hp: pokemon.stats[0].base_stat,
+        xp: pokemon.base_experience,
+        attack: pokemon.stats[1].base_stat,
+        defense: pokemon.stats[2].base_stat,
+        special: {
+            attack: pokemon.stats[3].base_stat,
+            defense: pokemon.stats[4].base_stat
+        },
+        /*TODO: FIX THIS 
+        moves: {
+            firstMove: pokemon.moves[0].move.name,
+            secondMove: pokemon.moves[1].move.name,
+            thirdMovement: pokemon.moves[2].move.name,
+            fourthMovement: pokemon.moves[3].move.name
+        },*/
+        speed: pokemon.stats[5].base_stat,
+        type: pokemon.types[0].type.name,
+        img: pokemon.sprites.front_default
+    }
     pokemonContainer.innerHTML = `
-    <img src="${pokemon.sprites.front_default}" height="150" width="150" alt="front-sprite"></img>
-    <p id="name">Name: ${pokemon.name}</p>
+    <p id="name" class="text-uppercase fw-bold"><span class="fw-normal fst-italic fs-4">#${pokemonData.id}</span> ${pokemonData.name}</p>
+    <p id="type" class="text-capitalize fw-bold">${pokemonData.type}</p>
+    <div class="border border-3 border-dark rounded-circle mb-2">
+    <img src="${pokemonData.img}" height="100" width="100" alt="front_sprite" class="front-img"></img>
+    </div>
+    <div class="container border border-3 border-dark w-50 text-center">
+    <p class="fs-3 fw-bold text-uppercase border-bottom border-dark border-3">Base Stats</p>
+    <p id="hp" class="fs-3"><span class="fst-italic fs-4"><i class="fas fa-heart"></i></span> ${pokemonData.hp}</p>
+    <p id="xp"class="fs-3"><span class="fst-italic fs-4">XP</span> ${pokemonData.xp}</p>
+    <p id="att-def"class="fs-3"><span class="fst-italic fs-4"><i class="fas fa-fist-raised"></i>/<i class="fas fa-shield-alt"></i></span> ${pokemonData.attack}/${pokemonData.defense}</p>
+    <p id="sp-att-def"class="fs-3"><span class="fst-italic fs-4"> SP <i class="fas fa-fist-raised"></i>/ SP <i class="fas fa-shield-alt"></i></span> ${pokemonData.special.attack}/${pokemonData.special.defense}</p>
+    <p id="sp"class="fs-3"><span class="fst-italic fs-4"><i class="fas fa-shoe-prints"></i></span> ${pokemonData.speed}</p>
+    </div>
     `
 }
 
